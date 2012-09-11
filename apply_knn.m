48fec319529d2fb10544e0bcc2f841ac1363d51e
Patrick de Kok <patrick@cxiu.nl>
2012-09-11 13:59:59 +0200

    Computes the first k_max kNNs on the data set, split on .75-.25

--- apply_knn.m
+++ apply_knn.m
@@ -1,12 +1,10 @@
-function x = apply_knn(k_max)
-[training_data,labels,test_A, test_B] = preprocess_data;
+function confusion_matrices = apply_knn(training, labels, test_A, test_B, k_max)
+min_k = min(k_max)
+confusion_matrices = cell(size(k_max)(2))
 
-for k = 1:1:k_max,
-  net = knn(2, 2, k, training_data, labels);
+for k = k_max,
+  net = knn(2, 2, k, training, labels);
   [y_A, l_A] = knnfwd(net, test_A);
   [y_B, l_B] = knnfwd(net, test_B);
-  confusion = transpose([histc(l_A, [1,2]), histc(l_B, [1,2])]);
-  disp(k)
-  disp(confusion)
+  confusion_matrices(k - min_k + 1) = transpose([histc(l_A, [1,2]), histc(l_B, [1,2])]);
 end
-x = 0;
