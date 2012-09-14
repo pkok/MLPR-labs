3a9f35ed1f0ac09148817fc33dc1c3a38f008a9c
Patrick de Kok <patrick@cxiu.nl>
2012-09-14 12:55:11 +0200

    Extra words \o/

--- lab2/src/features_of.m
+++ lab2/src/features_of.m
@@ -1,3 +1,3 @@
 function feature_vector = features_of(file)
-features = {'[Pp][Ee][Nn][1Ii][5Ss]', '[Vv][!1Ii][@4Aa][Gg][Rr][@4Aa]', '[Cc][!1Ii][@4Aa][Ll][!1Ii][5Ss]', '[Cc][0Oo][Cc][Kk]', '[3Ee][Rr][3Ee][Cc][Tt][!1Ii][0Oo][Nn]'};
+features = {'[Pp][Ee][Nn][1Ii][5Ss]', '[Vv][!1IiXx][@4Aa][Gg][Rr][@4Aa]', '[Cc][!1IiXx][@4Aa][Ll][!1Ii][5Ss]', '[Cc][0Oo][Cc][Kk]', '[3Ee][Rr][3Ee][Cc][Tt][!1Ii][0Oo][Nn]'};
 feature_vector = presentre(file, features);
