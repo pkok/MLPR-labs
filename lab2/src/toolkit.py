030773d116e4477154f57d480ccf157b9fab2206
Maarten Inja <maarten.inja@gmail.com>
2012-09-25 18:01:52 +0200

    count words function added

--- lab2/src/toolkit.py
+++ lab2/src/toolkit.py
@@ -21,3 +21,19 @@ def countre(folder, regexps):
     for regexp in match_counts.iterkeys():
         match_counts[regexp] /= filecount
     return (match_counts, filecount)
+
+def count_words(directory):
+    """ returns a defaultdict with words as keys and
+    the number of occurences of a word in the files 
+    in the directory as value. """
+    word_list = defaultdict(int) 
+    directory_content = os.listdir(directory)
+    for f in directory_content:
+        words = open(directory + os.sep + f, 'r').read().split(' ')
+        for w in words:
+            word_list[w] += 1 
+    return word_list
+
+if __name__ == "__main__":
+    #print count_words('spam/train')
+    pass
