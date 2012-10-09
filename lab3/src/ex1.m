22524e7373237e79005214d204815f91cbf457ab
Patrick de Kok <patrick@cxiu.nl>
2012-10-09 17:56:26 +0200

    Okay, NOW it is correct.

--- lab3/src/ex1.m
+++ lab3/src/ex1.m
@@ -1,7 +1,17 @@
-function [confusion_matrix, error_rate] = ex1()
+% Computes the required results for the first exercise of lab assignment 3 for
+% the 2012 MSc AI course Machine Learning: Pattern Recognition.
+% When passed the optional parameter random_seed, the random generator for
+% shuffling the dataset is set. If not, Octave/Matlab will determine the seed.
+function [confusion_matrix, error_rate] = ex1(random_seed)
 
-% Setting the seed for the random generator.
-rand("seed", 42)
+if nargin < 1
+  random_seed = -1;
+end
+
+if random_seed > 0
+  % Setting the seed for the random generator.
+  rand("seed", random_seed);
+end
 
 [train_A, train_B, test_A, test_B] = preprocess_data(1, 0.7, 'banana.mat');
 test = [test_A; test_B];
@@ -27,8 +37,8 @@ pCx = [pCA; pCB];
 
 true_A = sum(((pCA > pCB) == test_labels(:, 1)) .* test_labels(:, 1));
 true_B = sum(((pCA <= pCB) == test_labels(:, 2)) .* test_labels(:, 2));
-false_A = size(train_B) - true_B;
-false_B = size(train_A) - true_A;
+false_A = size(test_B, 1) - true_B;
+false_B = size(test_A, 1) - true_A;
 
 confusion_matrix = [true_A, false_A; false_B, true_B];
 error_rate = 1 - ((true_A + true_B) / size(test, 1));
