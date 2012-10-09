88ed5a491d4f16a0c7e7fead9209855f2435e3de
Patrick de Kok <patrick@cxiu.nl>
2012-10-09 09:49:36 +0200

    Done with ex1!

--- lab3/src/ex1.m
+++ lab3/src/ex1.m
@@ -1,15 +1,46 @@
-function [mu, sigma, pC] = ex1()
+function [confusion_matrix, error_rate] = ex1()
 
-[train_A, train_B] = preprocess_data(0, 0.7, 'banana.mat')
+[train_A, train_B, test_A, test_B] = preprocess_data(0, 0.7, 'banana.mat');
+test = [test_A; test_B]
+test_labels = [ones(size(test_A, 1)), zeros(size(test_A, 1)); zeros(size(test_B, 1)), ones(size(test_B, 1))]
 
-mu_A = sum(train_A) ./ size(train_A, 1)
-mu_B = sum(train_B) ./ size(train_B, 1)
-mu = [mu_A; mu_B]
+figure();
+hold on;
+plot(train_A(:, 1), train_A(:, 2), '@+1');
+plot(train_B(:, 1), train_B(:, 2), '@x3');
+hold off;
 
-sigma_A = cov(train_A)
-sigma_B = cov(train_B)
-sigma = [sigma_A; sigma_B]
+mu_A = sum(train_A) ./ size(train_A, 1);
+mu_B = sum(train_B) ./ size(train_B, 1);
+mu = [mu_A; mu_B];
 
-pCA = mvnpdf(train_A, mu_A, sigma_A)
-pCB = mvnpdf(train_B, mu_B, sigma_B)
-pC = [pCA; pCB] % No, not correct! This is p(x_n | C_k)
+sigma_A = cov(train_A);
+sigma_B = cov(train_B);
+sigma = [sigma_A; sigma_B];
+
+pCA = mvnpdf(test, mu_A, sigma_A) .* (size(train_A, 1) / (size(train_A, 1) + size(train_B, 1)));
+pCB = mvnpdf(test, mu_B, sigma_B) .* (size(train_B, 1) / (size(train_A, 1) + size(train_B, 1)));
+pCx = [pCA; pCB];
+
+true_A = 0;
+false_A = 0;
+true_B = 0;
+false_B = 0;
+
+for i=1:size(test,1),
+  if pCA(i) > pCB(i)
+    if test_labels(i, 1) == 1
+      true_A += 1;
+    else
+      false_A += 1;
+  else
+    if test_labels(i, 2) == 1
+      true_B += 1;
+    else
+      false_B += 1;
+    end
+  end
+end
+
+confusion_matrix = [true_A, false_A; false_B, true_B]
+error_rate = 1 - ((true_A + true_B) / size(test, 1))
