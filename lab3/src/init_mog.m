5a32bb1bca708730089ff3858ff2d56704e98eba
Patrick de Kok <patrick@cxiu.nl>
2012-10-09 21:51:00 +0200

    Finished the initialization!

--- lab3/src/init_mog.m
+++ lab3/src/init_mog.m
@@ -1,1 +1,11 @@
+% Compute the weight, mean and covariance of C slices of X, and store them in
+% cells of structs in MOG respectively as 'PI', 'MU' and 'SIGMA'.
 function MOG = init_mog(X, C)
+
+MOG = cell(C, 1);
+step = size(X, 1) / C;
+
+for i=1:C
+  slice = X((1 + ((i-1) * step)):(i*step), :);
+  MOG{i} = struct('MU', mean(slice), 'SIGMA', cov(slice), 'PI', 1/C);
+end
