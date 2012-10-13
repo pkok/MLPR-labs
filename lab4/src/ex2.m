c466eeb2af079f306fb755e5ba9d80665595bf5f
Patrick de Kok <patrick@cxiu.nl>
2012-10-13 02:09:33 +0200

    Can't test yet, because gaussian_process is broken. Need to read first, before I can continue.

---
+++ lab4/src/ex2.m
@@ -0,0 +1,28 @@
+function ex2(random_seed)
+
+if nargin < 1
+  random_seed = 0;
+end
+
+if random_seed ~= 0
+  % Setting the seed for the random generator.
+  rand('seed', random_seed);
+end
+
+load('chirps.mat');
+
+chirps = chirps(randperm(size(chirps, 1)), :);
+train = chirps(1:12, :);
+test = chirps(13:15, :);
+
+figure()
+hold on;
+plot(train(:, 1), train(:, 2), 'r+');
+plot(test(:, 1), test(:, 2), 'bx');
+xlabel('Frequency of cricket chirps (Herz)');
+ylabel('Ambient temperature (degrees Fahrenheit)');
+for i=1:size(test, 1)
+  label = gaussian_process(train(:, 1), train(:, 2), squared_exponential(0.5, 0.5), 1e-7, test(i, 1));
+  plot(test(i, 1), label, 'mo');
+end
+hold off;
