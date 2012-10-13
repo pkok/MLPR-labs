d70bbf6c47430def9c4ab8af9a38b8bf2567b202
Patrick de Kok <patrick@cxiu.nl>
2012-10-13 02:19:20 +0200

    Stylistic improvement

--- lab4/src/ex2.m
+++ lab4/src/ex2.m
@@ -17,12 +17,16 @@ test = chirps(13:15, :);
 
 figure()
 hold on;
-plot(train(:, 1), train(:, 2), 'r+');
-plot(test(:, 1), test(:, 2), 'bx');
+
 xlabel('Frequency of cricket chirps (Herz)');
 ylabel('Ambient temperature (degrees Fahrenheit)');
+plot(train(:, 1), train(:, 2), 'r+');
+plot(test(:, 1), test(:, 2), 'bx');
+
+k = squared_exponential(0.5, 0.5);
 for i=1:size(test, 1)
-  label = gaussian_process(train(:, 1), train(:, 2), squared_exponential(0.5, 0.5), 1e-7, test(i, 1));
+  label = gaussian_process(train(:, 1), train(:, 2), k, 1e-7, test(i, 1));
   plot(test(i, 1), label, 'mo');
 end
+
 hold off;
